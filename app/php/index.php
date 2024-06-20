5b0d092f8aebbe927140bf715ad08982b0b24bc8
alexandergarbuz <alexander.garbuz@gmail.com>
2024-06-20 11:39:36 -0500

    Initial check in

---
+++ app/php/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+$title = "Docker PHP Set Up Template";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta>
+<title><?php echo $title?></title>
+</head>
+<body>
+
+<h1><?php echo $title?></h1>
+
+</body>
+</html>
